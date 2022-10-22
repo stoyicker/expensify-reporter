ee62d536b4786919244e9806414e986739b90ffc
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2022-10-22 17:19:37 +0200

    Refactor away credential management UI into CredentialsJPanel

--- src/main/java/root/Main.java
+++ src/main/java/root/Main.java
@@ -1,12 +1,9 @@
 package root;
 
 import com.squareup.moshi.Moshi;
-import root.credentials.CredentialManager;
-import root.credentials.CredentialPersistenceRunnable;
-import root.credentials.Credentials;
+import root.credentials.CredentialsJPanel;
 
 import javax.swing.*;
-import java.awt.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -14,47 +11,14 @@ public final class Main {
     public static void main(String[] args) {
         STORAGE_ROOT.toFile().mkdirs();
         JFrame jFrame = new JFrame();
-        JPanel jPanel = new JPanel();
-        jPanel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
-        jPanel.setLayout(new GridLayout(0, 2));
-        jFrame.add(jPanel, BorderLayout.CENTER);
+        Moshi moshi = new Moshi.Builder().build();
+        jFrame.add(new CredentialsJPanel(moshi));
+
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setTitle("Expense generator");
-
-        JPasswordField partnerUserIDField = new JPasswordField();
-        partnerUserIDField.setEchoChar('*');
-        JLabel partnerUserIDLabel = new JLabel("partnerUserID: ");
-        jPanel.add(partnerUserIDLabel);
-        jPanel.add(partnerUserIDField);
-
-        JPasswordField partnerUserSecretField = new JPasswordField();
-        partnerUserSecretField.setEchoChar('*');
-        JLabel partnerUserSecretLabel = new JLabel("partnerUserSecret: ");
-        jPanel.add(partnerUserSecretLabel);
-        jPanel.add(partnerUserSecretField);
-
-        JTextField employeeEmailField = new JTextField();
-        partnerUserSecretField.setEchoChar('*');
-        JLabel employeeEmailLabel = new JLabel("Employee e-mail: ");
-        jPanel.add(employeeEmailLabel);
-        jPanel.add(employeeEmailField);
-
-        Moshi moshi = new Moshi.Builder().build();
-        CredentialManager credentialManager = new CredentialManager(moshi);
-        Credentials credentials = credentialManager.readCredentials();
-        if (credentials != null) {
-            partnerUserIDField.setText(credentials.partnerUserID);
-            partnerUserSecretField.setText(credentials.partnerUserSecret);
-            employeeEmailField.setText(credentials.employeeEmail);
-        }
-
-        new Thread(new CredentialPersistenceRunnable(
-                credentialManager, partnerUserIDField, partnerUserSecretField, employeeEmailField
-        )).start();
-
         jFrame.pack();
         jFrame.setLocationRelativeTo(null);
-        jPanel.requestFocus();
+        jFrame.getRootPane().requestFocus();
         jFrame.setVisible(true);
     }
 
---
+++ src/main/java/root/credentials/CredentialsJPanel.java
@@ -0,0 +1,54 @@
+package root.credentials;
+
+import com.squareup.moshi.Moshi;
+
+import javax.swing.*;
+
+public final class CredentialsJPanel extends JPanel {
+
+    public CredentialsJPanel(Moshi moshi) {
+        setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JPanel titlePanel = new JPanel();
+        JLabel credentialsLabel = new JLabel("CREDENTIALS");
+        titlePanel.add(credentialsLabel);
+        add(titlePanel);
+
+        JPanel partnerUserIDPanel = new JPanel();
+        JPasswordField partnerUserIDField = new JPasswordField();
+        partnerUserIDField.setEchoChar('*');
+        JLabel partnerUserIDLabel = new JLabel("partnerUserID: ");
+        partnerUserIDPanel.add(partnerUserIDLabel);
+        partnerUserIDPanel.add(partnerUserIDField);
+        add(partnerUserIDPanel);
+
+        JPanel partnerUserSecretPanel = new JPanel();
+        JPasswordField partnerUserSecretField = new JPasswordField();
+        partnerUserSecretField.setEchoChar('*');
+        JLabel partnerUserSecretLabel = new JLabel("partnerUserSecret: ");
+        partnerUserSecretPanel.add(partnerUserSecretLabel);
+        partnerUserSecretPanel.add(partnerUserSecretField);
+        add(partnerUserSecretPanel);
+
+        JPanel partnerEmployeeEmailPanel = new JPanel();
+        JTextField employeeEmailField = new JTextField();
+        partnerUserSecretField.setEchoChar('*');
+        JLabel employeeEmailLabel = new JLabel("Employee e-mail: ");
+        partnerEmployeeEmailPanel.add(employeeEmailLabel);
+        partnerEmployeeEmailPanel.add(employeeEmailField);
+        add(partnerEmployeeEmailPanel);
+
+        CredentialManager credentialManager = new CredentialManager(moshi);
+        Credentials credentials = credentialManager.readCredentials();
+        if (credentials != null) {
+            partnerUserIDField.setText(credentials.partnerUserID);
+            partnerUserSecretField.setText(credentials.partnerUserSecret);
+            employeeEmailField.setText(credentials.employeeEmail);
+        }
+
+        new Thread(new CredentialPersistenceRunnable(
+                credentialManager, partnerUserIDField, partnerUserSecretField, employeeEmailField
+        )).start();
+    }
+}
